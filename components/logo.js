c1f79030ee6fcff36f446b1e474f6eeb101b5b95
David Lima <37056819+davivers@users.noreply.github.com>
2021-11-29 21:46:01 -0300

    Update logo.js

--- components/logo.js
+++ components/logo.js
@@ -20,7 +20,7 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/microchip${useColorModeValue('', '-dark')}.png`
+  const footPrintImg = `/images/microchip${useColorModeValue('-light', '-dark')}.png`
 
   return (
     <Link href="/">
