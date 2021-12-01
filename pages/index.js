0447eccc30ccbd7631392d6923fd4a12f0d21491
David Lima <37056819+davivers@users.noreply.github.com>
2021-11-30 23:04:03 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ const Page = () => {
              <Box
         borderRadius="lg"
         mb={6}
-        p={3}
+        p={5}
         textAlign="center"
         bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
       >
