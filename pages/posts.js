f3836fb0a8e7b084bbb2ce0f94da362dbfa7e6c0
David Lima <37056819+davivers@users.noreply.github.com>
2021-12-05 22:57:13 -0300

    Update posts.js

--- pages/posts.js
+++ pages/posts.js
@@ -2,6 +2,10 @@ import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/layouts/section'
 import { GridItem } from '../components/grid-item'
+import thumbone from '../public/images/thumbone.png'
+import thumbpaul from '../public/images/thumbpaul.png'
+
+
 
 const Posts = () => (
   <Layout title="Posts" >
@@ -11,6 +15,22 @@ const Posts = () => (
         Popular Posts
       
      </Heading>
+ <Section delay={0.1}>
+       <SimpleGrid columns={[1,2,2]} gap={6}>
+            <GridItem
+            title="Minha experiência de aprendizado na plataforma DIO"
+            thumbnail={thumbone}
+            href="/">
+            </GridItem>
+            <GridItem
+            title="Sheltered Instructions"
+            thumbnail={thumbpaul}
+            href="https://rhspatriotvoice.org/1975/opinion/sheltered-instruction/"
+            target="_blank">
+
+            </GridItem>
+         </SimpleGrid>
+     </Section>
     </Container>
   </Layout>
 )
