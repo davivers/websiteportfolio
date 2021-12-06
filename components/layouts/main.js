dcfd38d6e359da8467453ec81f506d2f09ebfad0
David Lima <37056819+davivers@users.noreply.github.com>
2021-12-06 09:38:35 -0300

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 import Navbar from '/components/navbar.js'
 import { Box, Container } from '@chakra-ui/react'
-import catfav from '../public/images/catfav.png
 
 const Main = ({children, router}) => {
     return (
         <Box as="main" pb={8}>
             <Head>
-                <link rel="icon" type="image/png" sizes="64x64" href={catfav} />
+               
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>David Lima - Portfolio</title>
+                <link rel="icon" type="image/png" sizes="64x64" href="/public/images/catfav.png" />
 
             </Head>
 
